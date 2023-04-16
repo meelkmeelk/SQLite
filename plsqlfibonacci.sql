e313cb7f7b593da6fa2fb1bc2d2195c49ba632f5
meelkmeelk <80961808+meelkmeelk@users.noreply.github.com>
2023-04-16 23:59:54 +0800

    Update plsqlfibonacci.sql

--- plsqlfibonacci.sql
+++ plsqlfibonacci.sql
@@ -8,16 +8,16 @@ DECLARE
       ELSIF x = 1 THEN
         RETURN 1;
       ELSE
-        RETURN fibonacci(x - 1) + fibonacci(x - 2);
+        RETURN fibonacci(x - 1) + fibonacci(x - 2); /* this is a recurision way of fibonacci*/
       END IF;
     END;
 BEGIN
-  DBMS_OUTPUT.PUT_LINE('Fibonacci sequence up to ' || n || ':');
+  DBMS_OUTPUT.PUT_LINE('Fibonacci sequence up to ' || n || ':'); /* umm it means to display the number*/
   
-  FOR i IN 0..n LOOP
+  FOR i IN 0..n LOOP /* same as python just slightly different way of looping */
     IF fibonacci(i) > n THEN
       EXIT;
     END IF;
-    DBMS_OUTPUT.PUT(fibonacci(i) || ' ');
+    DBMS_OUTPUT.PUT(fibonacci(i) || ' '); /* btw for ppl who dont know || means concatenate*//
   END LOOP;
 END;
